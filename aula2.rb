3b1b933ff62c3eca3e521f44fbe7eff8d4517a5f
Rayane Brasili <107143826+rayanebrasili@users.noreply.github.com>
2022-12-06 17:16:52 -0300

    Add files via upload

---
+++ aula2.rb
@@ -0,0 +1,11 @@
+# saida de dado
+print "Digite seu nome:"
+
+# Recebendo entrada do teclado
+
+name = gets.chomp
+
+# Saida utilizando puts
+# Use códigos ruby dentro de uma string com #{code}
+
+puts "Hello #{name}!"
